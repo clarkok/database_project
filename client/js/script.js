91103a057195c699f4fb4248c656232850513a63
Clarkok Zhang <mail@clarkok.com>
2015-04-02 15:09:43 +0800

    detailed query page

--- client/js/script.js
+++ client/js/script.js
@@ -4,13 +4,14 @@ var route = {};
 
 var $wrapper = $('#wrapper');
 
+var books = {};
+
 // query
 (function ($, w) {
-  var books = {};
-  var $list = $('#query-content ul');
+  var $list = $('#query-main');
 
-  var buildBook = function (book) {
-    return $('<li />').append(
+  var buildBookList = function (book) {
+    return $('<li />').attr('id', 'b' + book.bid).append(
       $('<div />').addClass('column bid').text(book.bid),
       $('<div />').addClass('column category').text(book.category),
       $('<div />').addClass('column title').text(book.title),
@@ -20,28 +21,70 @@ var $wrapper = $('#wrapper');
       $('<div />').addClass('column price').text(book.price),
       $('<div />').addClass('column total').text(book.total),
       $('<div />').addClass('column stock').text(book.stock)
-    );
+    ).data('original', book);
   };
+
+  var buildBook = buildBookList;
 
   var insertBook = function ($book) {
-    if ()
+    var $current = $list.last();
+
+    // find the property index
+    while ($current.data('original') 
+      && $current.data('original').bid > $book.data('original').bid) {
+      $current = $current.prev('li');
+    }
+
+    $current.append($book);
+
+    // to start transition
+    setTimeout(function () {
+      $book.addClass('show');
+    }, 15);
   };
 
-  Object.observe(books, function (changes) {
-    changes.forEach(function (change) {
-      switch (change.type) {
-        case "add":
-        case "update":
-        case "delete":
-        default:
-      };
+  var removeBook = function (book) {
+    var $book = $list.find('#b' + book.bid).attr('id', '');
+    $book.get(0).className = 'hide';
+    $book.on('transitionend', function () {
+      $book.remove();
     });
-  });
+  };
+
+  var updateBooks = function (changes) {
+    if (changes === undefined) {
+      $list.find('.show').remove();
+      Object.getOwnPropertyNames(books).forEach(function (item) {
+        insertBook(buildBook(item));
+      });
+    }
+    else {
+      changes.forEach(function (change) {
+        switch (change.type) {
+          case "add":
+            insertBook(buildBook(change.object[change.name]));
+            break;
+          case "update":
+            removeBook(change.oldValue);
+            insertBook(buildBook(change.object[change.name]));
+            break;
+          case "delete":
+            removeBook(change.oldValue);
+            break;
+          default:
+        };
+      });
+    }
+  };
 
   route['query'] = {
     init : function () {
+      Object.observe(books, updateBooks);
+      updateBooks();
     },
     deinit : function () {
+      console.log('dinit');
+      Object.unobserve(books, updateBooks);
     }
   };
 })(window.jQuery, window);
@@ -77,8 +120,11 @@ var $wrapper = $('#wrapper');
     var query_body = matched[3];
 
     if (route.hasOwnProperty(original_hash)) {
+      console.log('hash deinit');
       route[original_hash].deinit();
     }
+
+    original_hash = hash_name;
 
     if (route.hasOwnProperty(hash_name)) {
       $wrapper.get(0).className = hash_name;
