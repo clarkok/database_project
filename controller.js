03b755112a4e3fb6ddfa846a1e036eb5963d1159
MForever78 <shunjian1128@gmail.com>
2015-04-15 10:40:29 +0800

    fix undefined error

--- controller.js
+++ controller.js
@@ -7,7 +7,9 @@ var controller = function(server) {
 
     // client request a query
     socket.on('query', function(query) {
-      query.data.aid = socket.request.session.aid;
+      if (socket.request.session && socket.request.session.aid) {
+        query.data.aid = socket.request.session.aid;
+      }
       model.query(query)
         .then(function(result) {
           socket.emit('query', result);
