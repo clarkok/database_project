8c21a5a28ce417f9e0dd3cee3d4bb0cd545fe8cd
Clarkok Zhang <mail@clarkok.com>
2015-04-18 20:55:58 +0800

    rewrite socket event

---
+++ client/js/socket_event.js
@@ -0,0 +1,30 @@
+"use strict";
+
+var s = window.io(window.location.origin);
+
+(function ($, w) {
+  w.socketEvents = {
+    name_map : {},
+    addListener : function (name, cb) {
+      if (this.name_map.hasOwnProperty(name))
+        this.name_map[name].push(cb);
+      else
+        this.name_map[name] = [cb];
+    },
+    trigger : function (name, data) {
+      if (this.name_map.hasOwnProperty(name))
+        this.name_map[name].forEach(function (item) {
+          item.call(window, data);
+        });
+    }
+  };
+
+  s.on('query', function (d) {
+    console.log(d);
+    w.socketEvents.trigger(d.action, d.data);
+  });
+
+  s.on('error', function (d) {
+    console.log(d);
+  });
+})(window.jQuery, window);
